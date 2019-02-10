03b2d952af5f63e1f88483766dc6a15ea53dc72c
CloudRex <cloudrex@outlook.com>
2019-02-09 19:40:24 -0500

    Standarize AST structures

--- ast.go
+++ ast.go
@@ -1,10 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-// AST : Represents the AST generator.
-type AST struct {
-	parser *Parser
+	"github.com/llir/llvm/ir"
+)
+
+// Ast : Represents the AST generator.
+type Ast struct {
+	parser        *Parser
+	stashedParser *Parser
+}
+
+// AstBlockNode : Base interface for an AST block node.
+type AstBlockNode interface {
+	create(block *ir.Block)
+}
+
+// AstModuleNode : Base interface for an AST module node.
+type AstModuleNode interface {
+	create(module *ir.Module)
 }
 
 // IdentifierAST : Represents the identifier AST node.
@@ -12,9 +27,51 @@ type IdentifierAST struct {
 	name string
 }
 
-// StatementAST : Represents a statement AST node.
-type StatementAST struct {
-	tokens []Token
+// OperandKind : Represents the type of operand used in an operation.
+type OperandKind = int
+
+const (
+	// OperandKindIdentifier : Represents an identifier operand.
+	OperandKindIdentifier OperandKind = 0
+
+	// OperandKindLiteral : Represents a literal value operand.
+	OperandKindLiteral OperandKind = 1
+)
+
+// OperandAST : Represents an operand AST node.
+type OperandAST struct {
+	value string
+	kind  OperandKind
+}
+
+// OperatorKind : Represents the type of an operator.
+type OperatorKind = int
+
+const (
+	// OperatorKindAdd : Represents the addition operator type.
+	OperatorKindAdd OperatorKind = 0
+
+	// OperatorKindSubstract : Represents the substraction operator type.
+	OperatorKindSubstract OperatorKind = 1
+
+	// OperatorKindMultiply : Represents the multiplication operator type.
+	OperatorKindMultiply OperatorKind = 1
+
+	// OperatorKindDivide : Represents the division operator type.
+	OperatorKindDivide OperatorKind = 1
+)
+
+// OperatorAST : Represents an operator AST node.
+type OperatorAST struct {
+	value string
+	kind  OperatorKind
+}
+
+// BinaryOperationAST : Represents a binary operation AST node.
+type BinaryOperationAST struct {
+	leftSide  OperandAST
+	rightSide OperandAST
+	operator  OperatorAST
 }
 
 // ExpressionAST : Represents an expression AST node.
@@ -22,8 +79,26 @@ type ExpressionAST struct {
 	tokens []Token
 }
 
+// BlockAST : Represents a statement block AST node.
+type BlockAST struct {
+	name       string
+	statements []AstBlockNode
+}
+
+func (block *BlockAST) create() *ir.Block {
+	irBlock := ir.NewBlock(block.name)
+
+	for i := 0; i < len(block.statements); i++ {
+		statement := block.statements[i]
+
+		statement.create(irBlock)
+	}
+
+	return irBlock
+}
+
 // Process and validate function.
-func (ast *AST) function() {
+func (ast *Ast) function() {
 	// Must be followed by an identifier.
 	if ast.parser.peek().kind != TokenKindIdentifier {
 		ast.parser.fatal("Expecting identifier after function definition keyword")
@@ -45,7 +120,7 @@ func (ast *AST) function() {
 }
 
 // Process and validate function arguments.
-func (ast *AST) functionArgs() {
+func (ast *Ast) functionArgs() {
 	derived := ast.parser.derive()
 
 	for token := derived.get(); token.kind != TokenKindParenEnd; token = derived.next() {
@@ -57,23 +132,54 @@ func (ast *AST) functionArgs() {
 	}
 }
 
+// Save the current parser and apply a new parser.
+func (ast *Ast) applyParser(parser *Parser) *Ast {
+	ast.stashedParser = ast.parser
+	ast.parser = parser
+
+	return ast
+}
+
+// Revert the current parser to a previously stashed parser.
+func (ast *Ast) revertParser() *Ast {
+	ast.parser = ast.stashedParser
+
+	return ast
+}
+
 // Process and validate a statement block.
-func (ast *AST) block() {
+func (ast *Ast) block() BlockAST {
 	token := ast.parser.next()
 
 	if token.kind != TokenKindBlockStart {
 		ast.parser.fatal("Expecting block start: '{'")
 	}
 
+	tokens := ast.parser.until(TokenKindBlockEnd)
+	parser := newParser(tokens)
+
+	ast.applyParser(parser)
+
+	var statements []AstBlockNode
+
 	// TODO.
+	for i := 0; i < len(tokens); i++ {
+		statements = append(statements, ast.statement())
+	}
+
+	ast.revertParser()
+
+	return BlockAST{name: "anonymous_block"}
 }
 
-func (ast *AST) expression() ExpressionAST {
-	// TODO.
-}
-
-func (ast *AST) statement() StatementAST {
+func (ast *Ast) statement() AstBlockNode {
 	tokens := ast.parser.until(TokenKindSemiColon)
+
+	if len(tokens) == 1 || len(tokens) == 1 { // Empty statement.
+		// TODO.
+	}
+
+	var node AstBlockNode
 
 	// Validate captured tokens.
 	for i := 0; i < len(tokens); i++ {
@@ -85,11 +191,16 @@ func (ast *AST) statement() StatementAST {
 		}
 	}
 
-	return StatementAST{tokens: tokens}
+	return node
+}
+
+func (ast *Ast) expression() ExpressionAST {
+	// TODO.
+	panic("Not yet implemented")
 }
 
 // Process and validate an identifier.
-func (ast *AST) identifier() IdentifierAST {
+func (ast *Ast) identifier() IdentifierAST {
 	token := ast.parser.get()
 
 	if !isIdentifier(token.value) {
